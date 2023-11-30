6e4a17a4a48983a9b2d9f57c0311444b838535a3
harshdevarakonda <ridepulsarns@gmail.com>
2023-11-30 22:23:30 +0530

    my tenth commit

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs=[
+    {
+        name:'Bujji Bangaram',
+        path:'musics/bujji bangaram.mp3',
+        artist:'~Arman Malik,Yamini',
+        cover:'images/bujji bangaram poster.png'
+    },
+    {
+        name:'Sithravathi',
+        path:'musics/sittarala sitravati.mp3',
+        artist:'~Rahul sipligunj,ramya Behara',
+        cover:'images/sithravathi poster.png'
+    },
+    {
+        name:'Leelammo',
+        path:'musics/leelammo.mp3',
+        artist:'~Nakash Aziz,Indravathi',
+        cover:'images/leelammo poster.png'
+    },
+    {
+        name:'Uyyalo Uyyaala',
+        path:'musics/uyyalo uyyala.mp3',
+        artist:'~S.P. Charan',
+        cover:'images/uyyalo uyyala poster.png'
+    },
+    {
+        name:'Roar of Kesari',
+        path:'musics/roar of kesari.mp3',
+        artist:'~Thaman S',
+        cover:'images/roar of kesari poster.png'
+    },
+]
